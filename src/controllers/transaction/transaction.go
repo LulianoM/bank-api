f0173b011d6152897a3f161e1cac22612a050155
LulianoM <martinslucianofigueira@gmail.com>
2023-07-09 23:22:03 -0300

    feat: add validation account in controllers

--- src/controllers/transaction/transaction.go
+++ src/controllers/transaction/transaction.go
@@ -1,6 +1,8 @@
 package transaction
 
 import (
+	"errors"
+
 	"github.com/LulianoM/bank-api/src/interfaces"
 	"github.com/LulianoM/bank-api/src/models"
 	"github.com/LulianoM/bank-api/src/repositories"
@@ -8,12 +10,21 @@ import (
 
 type TransactionController struct {
 	transactionRepositories interfaces.TransactionRepositories
+	accountRepositories     interfaces.AccountRepositories
 }
 
 func NewTransactionController(repositories *repositories.RepositoriesContainer) *TransactionController {
-	return &TransactionController{transactionRepositories: repositories.TransactionRepositories}
+	return &TransactionController{
+		transactionRepositories: repositories.TransactionRepositories,
+		accountRepositories:     repositories.AccountRepositories,
+	}
 }
 
 func (tc *TransactionController) Create(transaction models.Transaction) error {
+
+	if !tc.accountRepositories.AccountExists(transaction.AccountID) {
+		return errors.New("account dont exists")
+	}
+
 	return tc.transactionRepositories.Create(transaction)
 }
