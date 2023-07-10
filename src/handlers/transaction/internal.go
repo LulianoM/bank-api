cc0579a4f4a8a9f9a138483be396cb5508b58c8b
LulianoM <martinslucianofigueira@gmail.com>
2023-07-09 23:21:39 -0300

    feat: handler transaction

--- src/handlers/transaction/internal.go
+++ src/handlers/transaction/internal.go
@@ -1,7 +1,29 @@
 package transaction
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"net/http"
+	"time"
+
+	"github.com/LulianoM/bank-api/src/models"
+	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
+)
 
 func (th *TransactionHanlder) Create(c *fiber.Ctx) error {
-	return nil
+	var newTransaction models.Transaction
+
+	if err := c.BodyParser(&newTransaction); err != nil {
+		return c.Status(http.StatusBadRequest).JSON(err)
+	}
+
+	newTransaction.ID = uuid.New()
+	newTransaction.EventDate = time.Now()
+
+	// validate bodys
+
+	if err := th.transactionController.Create(newTransaction); err != nil {
+		return c.Status(http.StatusBadRequest).JSON(err)
+	}
+
+	return c.Status(http.StatusOK).JSON(newTransaction)
 }
